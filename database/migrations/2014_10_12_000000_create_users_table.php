8827dae6baa22715015e681e30fa228ed6ae1b24
modithapahan <modithapahan0@gmail.com>
2023-05-12 00:47:20 +0530

    create database models

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,11 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->bigInteger('id')->autoIncrement();
+            $table->string('name')->nullable();
+            $table->string('email')->unique()->always();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->integer('role_id')->nullable();
+            $table->string('password')->always();
+            $table->integer('age')->nullable();
+            $table->string('address')->nullable();
+            $table->string('contact')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
