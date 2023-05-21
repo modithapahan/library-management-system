bf48fdba7c8c6cdb84ced2da8f708ca289b36443
modithapahan <modithapahan0@gmail.com>
2023-05-21 14:06:23 +0530

    register view updated

--- resources/views/pages/user/register.blade.php
+++ resources/views/pages/user/register.blade.php
@@ -28,24 +28,32 @@
             {{-- https://media.istockphoto.com/id/1281150061/vector/register-account-submit-access-login-password-username-internet-online-website-concept.jpg?s=612x612&w=0&k=20&c=9HWSuA9IaU4o-CK6fALBS5eaO1ubnsM08EOYwgbwGBo= --}}
         </div>
         
-        <div class="flex-grow-1 ms-3">
-            <form>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" name="password" id="exampleInputPassword1">
-                </div>
-                <center>
-                    <button type="submit" 
-                        class="btn btn-primary" 
-                        style="width: 50%; letter-spacing: 0.1em; font-weight: 700;"
-                    > Register
-                    </button>
-                </center>
-            </form>
+        <div class="flex-grow-1 ms-3 mb-3" 
+            style="display: flex; justify-content: center; align-items: center"
+        >
+            <div style="max-width: 100%">
+                <form>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Email address</label>
+                        <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                        <input type="password" class="form-control" name="password" id="exampleInputPassword1">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                        <input type="password" class="form-control" name="confirm-password" id="exampleInputPassword1">
+                    </div>
+                    <center>
+                        <button type="submit" 
+                            class="btn btn-primary" 
+                            style="width: 50%; letter-spacing: 0.1em; font-weight: 700;"
+                        > Register
+                        </button>
+                    </center>
+                </form>
+            </div>
         </div>
     </div>
 </body>
